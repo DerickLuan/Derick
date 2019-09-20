b8b3af6e32be4ea574092853c34e84a31ea75d52
DerickLuan <44878877+DerickLuan@users.noreply.github.com>
2019-09-20 19:07:09 -0400

    Add files via upload

---
+++ Ex3.3/Leitura.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+import com.mysql.cj.jdbc.Driver;
+import java.util.ArrayList;
+
+public class Leitura
+{
+	public ArrayList<String> salvar(ArrayList<String> p, int op)
+	{
+		p.add(0, JOptionPane.showInputDialog(null, "Nome: ", "Salvar", JOptionPane.QUESTION_MESSAGE));
+		p.add(1, JOptionPane.showInputDialog(null, "Idade: ", "Salvar", JOptionPane.QUESTION_MESSAGE));
+		p.add(2, JOptionPane.showInputDialog(null, "CPF: ", "Salvar", JOptionPane.QUESTION_MESSAGE));
+		p.add(3, JOptionPane.showInputDialog(null, "Sexo: ", "Salvar", JOptionPane.QUESTION_MESSAGE));
+		if(op == 1)
+		{
+			p.add(4, JOptionPane.showInputDialog(null, "Siape: ", "Salvar", JOptionPane.QUESTION_MESSAGE));
+		}
+		else if(op == 2)
+		{
+			p.add(4, JOptionPane.showInputDialog(null, "RA: ", "Salvar", JOptionPane.QUESTION_MESSAGE));
+		}
+		return p;
+	}
+
+	public String nome()
+	{
+		String nome = JOptionPane.showInputDialog(null, "Nome: ", "Nome", JOptionPane.QUESTION_MESSAGE);
+		return nome;
+	}
+	public String cpf()
+	{
+		String cpf = JOptionPane.showInputDialog(null, "CPF: ", "CPF", JOptionPane.QUESTION_MESSAGE);
+		return cpf;
+	}
+}
